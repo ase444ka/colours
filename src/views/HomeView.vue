410c6261d15dc5db97b2f134b39c93e988b7422f
Ase444ka <istomina.asia@yandex.ru>
2024-11-18 15:47:15 +0300

    add layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,14 +5,21 @@ import SliderComponent from '@/components/SliderComponent.vue'
 <template>
   <main>
     <SliderComponent />
-    <div class="filters">фильтры</div>
-    <div class="catalog">
-      <div class="catalog__title">товары</div>
-      <div class="products">список товаров</div>
+    <div class="container">
+      <div class="content">
+        <div class="content__filters">фильтры</div>
+        <div class="content__catalog">
+          <div class="catalog__title">товары</div>
+          <div class="catalog__products">список товаров</div>
+        </div>
+      </div>
     </div>
   </main>
 </template>
 
 <style lang="scss">
-
+.content {
+  display: grid;
+  grid-template-columns: 150px 1fr;
+}
 </style>
